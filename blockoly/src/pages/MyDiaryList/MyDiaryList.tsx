85daea50623a8ebcb596c7d7c79fda0a862b06c6
Neru-neru <yumadachi0113soccer@gmail.com>
2022-05-02 00:26:44 +0900

    make mydiarylist

--- blockoly/src/pages/MyDiaryList/MyDiaryList.tsx
+++ blockoly/src/pages/MyDiaryList/MyDiaryList.tsx
@@ -1,5 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const MyDiaryList: React.FC = () => <div />
+type DiaryInfo = {
+  diaryId: string
+  title: string
+  writerId: string
+  writerName: string
+  description: string
+  thumbnailBody: string
+}
+
+type DiaryInfoList = {
+  diaryCount: number
+  items: DiaryInfo[]
+}
+
+const MyDiaryList: React.FC = () => {
+  const today = new Date()
+  const [date, setDate] = useState({ year: today.getFullYear(), month: today.getMonth() })
+  const [DiaryInfoList, setDiaryInfoList] = useState<DiaryInfo[]>()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result: DiaryInfoList = await axios.get(`url`, {
+        params: {
+          year: date.year,
+          month: date.month,
+        },
+      })
+      setDiaryInfoList(result.items)
+    }
+    fetchData()
+  }, [date])
+
+  const pickDateOnCalendar = (year: number, month: number) => {
+    setDate({ year, month })
+  }
+
+  return <div />
+}
 
 export default MyDiaryList
