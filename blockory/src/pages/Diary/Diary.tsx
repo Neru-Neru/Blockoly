bf67f48f946a7f7bbd7b3b296f16992ad01608c8
Neru-neru <yumadachi0113soccer@gmail.com>
2022-06-04 18:23:15 +0900

    fix design

--- blockory/src/pages/Diary/Diary.tsx
+++ blockory/src/pages/Diary/Diary.tsx
@@ -1,10 +1,12 @@
+import FlatButton from 'components/FlatButton/FlatButton'
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 import DiaryCode from './DiaryCode/DiaryCode'
 import DiaryContent from './DiaryContent/DiaryContent'
 
 const Diary: React.FC = () => {
+  const navigate = useNavigate()
   const location = useLocation()
 
   const state = location.state as { diary: DiaryInfo }
@@ -12,20 +14,22 @@ const Diary: React.FC = () => {
 
   console.log('Diary', diary)
 
+  const backPage = () => {
+    navigate(-1)
+  }
+
   return (
     <div className="container">
-      <div className="row">
+      <div className="row h-75">
         <div className="col-6">
           <DiaryContent title={diary.Title} description={diary.Description} /> <DiaryCode diaryBody={diary.DiaryBody} />
         </div>
         <div className="col-6">
           <img src={`data:image/png;${diary.ThumbnailBody}`} alt={diary.Title} />
         </div>
-        <div>
-          {/* <button onClick={() => props.history.goBack()} className="btn btn-outline-secondary">
-            もどる
-          </button> */}
-        </div>
+      </div>
+      <div>
+        <FlatButton text="もどる" onClick={backPage} className="btn btn-outline-secondary" />
       </div>
     </div>
   )
--- blockory/src/pages/Diary/DiaryContent/DiaryContent.tsx
+++ blockory/src/pages/Diary/DiaryContent/DiaryContent.tsx
@@ -10,10 +10,10 @@ const DiaryContent: React.FC<Props> = (props) => {
 
   return (
     <>
-      <div className="my-3 p-3 border rounded-pill d-flex align-items-center justify-content-center bg-white">
+      <div className="my-3 p-2 border d-flex align-items-center justify-content-center bg-white">
         <h4 className="text-center">{title}</h4>
       </div>
-      <div className="p-3 border h-75 rounded-3 bg-white">
+      <div className="p-3 border h-25 bg-white">
         <p>{description}</p>
       </div>
     </>
